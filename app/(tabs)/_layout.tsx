909962a0e43dfac8e8073e7a9df475b5e269da99
safitrih <miraayusaftri@gmail.com>
2024-12-23 12:51:07 +0700

    halaman home

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+export default () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#FF4500', // Warna ikon saat aktif
+        tabBarInactiveTintColor: '#808080', // Warna ikon saat tidak aktif
+      }}
+    >
+      {/* Halaman Home dengan Tab Navigasi */}
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+          title: 'Home', // Menyesuaikan judul tab
+        }}
+      />
+    </Tabs>
+  );
+};
